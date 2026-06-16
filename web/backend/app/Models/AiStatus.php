a04d2526eadd567e43c9b23a03bf94a52cb699b3
髙木陽成 <ta19ga789@gmail.com>
2026-06-16 20:25:44 +0900

    SoftDeletesのuse分の追加

--- web/backend/app/Models/AiStatus.php
+++ web/backend/app/Models/AiStatus.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AiStatus extends Model
 {
-    use HasFactory;
-    use $table->softDeletes();
+    use HasFactory, SoftDeletes;
 }
--- web/backend/app/Models/KakeiboDefaultCategory.php
+++ web/backend/app/Models/KakeiboDefaultCategory.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class KakeiboDefaultCategory extends Model
 {
-    use HasFactory;
-    use $table->softDeletes();
+    use HasFactory, SoftDeletes;
 }
