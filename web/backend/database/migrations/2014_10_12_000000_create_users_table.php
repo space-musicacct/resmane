3870712d40f04cae88e69f66e782afb111ed5d73
髙木陽成 <ta19ga789@gmail.com>
2026-06-16 15:26:38 +0900

    login_idとemailに一意制約の付与

--- web/backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ web/backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('login_id', 15);
-            $table->string('email', 255);
+            $table->string('login_id', 15)->unique();
+            $table->string('email', 255)->unique();
             $table->string('name', 50);
             $table->string('password_hash', 255);
             $table->timestamps();
