499d56761a730c992bd4042709965e45267a1823
髙木陽成 <ta19ga789@gmail.com>
2026-06-16 20:10:34 +0900

    モデルクラスの作成 userモデルは変更なし

---
+++ web/backend/app/Models/UpperLimitSetting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UpperLimitSetting extends Model
+{
+    use HasFactory;
+    use $table->softDeletes();
+}
